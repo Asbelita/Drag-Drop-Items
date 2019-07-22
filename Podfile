da69dc0c4508c1453b774dee5769dbf7b2649b05
Manuel Bueno <manuel.mebm@gmail.com>
2019-07-22 10:39:32 -0500

    Added password recovery, pod to make view scroll to input when its too low on the screen.

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WhouseApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for WhouseApp
+  pod "KeyboardAvoidingView", '~> 4.0'
+
+
+end
---
+++ WhouseApp/PasswordRecoveryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordRecoveryViewController.swift
+//  WhouseApp
+//
+//  Created by Manuel Bueno on 7/19/19.
+//  Copyright © 2019 Nelumbo. All rights reserved.
+//
+
+import UIKit
+
+class PasswordRecoveryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+    }
+}
